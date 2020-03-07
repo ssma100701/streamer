c79a5e39190905747f61427725c25a26ddc6a07f
ssma100701 <ssma100701@gmail.com>
2020-03-08 00:45:18 +1100

    oauth with google

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StreamCreate = () => {
+  return <div>SteamCreate</div>;
+};
+
+export default StreamCreate;
---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StreamEdit = () => {
+  return <div></div>;
+};
+
+export default StreamEdit;
