eaad4d82260748c28c22bdef2b2af3b6280f64c2
ssma100701 <ssma100701@gmail.com>
2020-03-10 02:23:38 +1100

    show page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -13,13 +13,13 @@ const App = () => {
     <div className='ui container'>
       <Router history={history}>
         <Header />
-        <div>
+        <Switch>
           <Route path='/' exact component={StreamList} />
           <Route path='/stream/new' exact component={StreamCreate} />
           <Route path='/stream/edit/:id' exact component={StreamEdit} />
           <Route path='/stream/delete/:id' exact component={StreamDelete} />
           <Route path='/stream/show/:id' exact component={StreamShow} />
-        </div>
+        </Switch>
       </Router>
     </div>
   );
